8d2af673a7ac2d2f4e7889c9129e38866fd3333b
Sean Porter <portertech@gmail.com>
2012-05-28 21:50:42 -0700

    cookbook dependencies

--- metadata.rb
+++ metadata.rb
@@ -4,3 +4,12 @@ license          "MIT"
 description      "Chef cookbook for multi-tenant Wordpress hosting"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
+
+%w[
+  php
+  apache2
+  mysql
+  vsftpd
+].each do |cookbook|
+  depends cookbook
+end
