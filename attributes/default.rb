f177afcfadc9af8dde079bcb9f0c3fcd23d34db2
Sean Porter <portertech@gmail.com>
2012-05-28 21:31:54 -0700

    attributes and an lwrp

---
+++ attributes/default.rb
@@ -0,0 +1,4 @@
+# wordpress
+default.wordpress.version = "3.1.2"
+default.wordpress.checksum = "1006a1bb97b42381ad82490d00d9b7fb9f7a1c9d83ee2ed36935a9eb99c81064"
+default.wordpress.root = "/opt/wordpress"
