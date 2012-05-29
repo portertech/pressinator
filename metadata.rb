1eac3227cc5af66ae41f1535fbdd11197781b7c3
Sean Porter <portertech@gmail.com>
2012-05-28 19:00:00 -0700

    bootstrap cookbook

---
+++ metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Sean Porter Consulting"
+maintainer_email "portertech@gmail.com"
+license          "MIT"
+description      "Chef cookbook for multi-tenant Wordpress hosting"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
