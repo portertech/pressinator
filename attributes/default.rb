4c7aa71d932cc3bd6bb7e35e5e5c27596d66a2db
Sean Porter <portertech@gmail.com>
2012-05-30 20:17:31 -0700

    set mysql passwords in attributes, intended to be overridden

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,8 @@
+# mysql
+default.mysql.server_debian_password = "secret"
+default.mysql.server_root_password = "secret"
+default.mysql.server_repl_password = "secret"
+
 # wordpress
 default.wordpress.root = "/opt/wordpress"
 default.wordpress.version = "3.1.2"
